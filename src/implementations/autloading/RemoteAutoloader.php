7e910ca899e77be36f9d447e4f40e91d5cd68406
Till Wehowski <github@webfan.de>
2020-12-17 00:31:10 +0100

    Update RemoteAutoloader.php

--- src/implementations/autloading/RemoteAutoloader.php
+++ src/implementations/autloading/RemoteAutoloader.php
@@ -1,23 +1,23 @@
 <?php
 namespace frdl\implementation\psr4;
 
-
-$oldc = file_get_contents(__FILE__);
-$code = file_get_contents('https://frdl.webfan.de/install/?salt='.sha1(mt_rand(1000,9999)).'&source=\frdl\implementation\psr4\RemoteAutoloader');
-try{
-if(false === $code){
-  throw new \Exception(sprintf('Could not load %s from %s', \frdl\implementation\psr4\RemoteAutoloader::class, 'frdlwebfan.de'));
-}
+function ___loadFreshSourceFromServer(){
+   $oldc = file_get_contents(__FILE__);
+   $code = file_get_contents('https://frdl.webfan.de/install/?salt='.sha1(mt_rand(1000,9999)).'&source=\frdl\implementation\psr4\RemoteAutoloader');
+   try{
+ if(false === $code){
+   throw new \Exception(sprintf('Could not load %s from %s', \frdl\implementation\psr4\RemoteAutoloader::class, 'frdlwebfan.de'));
+ }
 
   file_put_contents(__FILE__, $code);
  
- return require __FILE__;
+// return require __FILE__;
 
 }catch(\Exception $e){
-   file_put_contents(__FILE__, $oldc);
-    throw $e;
+     file_put_contents(__FILE__, $oldc);
+     throw $e;
+  }
 }
-
 
 
 class RemoteAutoloader
@@ -41,6 +41,8 @@ class RemoteAutoloader
 	
 	
    public function __construct($server = 'frdl.webfan.de', $register = true, $version = 'latest', $allowFromSelfOrigin = false, $salted = false, $classMap = null, $cacheDir = null, $cacheLimit = null){
+	        ___loadFreshSourceFromServer();
+	   
 	        $this->withSalt($salted);
 	        $this->withClassmap($classMap);
 		$this->allowFromSelfOrigin = $allowFromSelfOrigin;
@@ -447,7 +449,9 @@ class RemoteAutoloader
 		$code =rtrim($code, '?php>');	
 		eval($code);
 		return true;	
-	}
+	}else{
+	      throw new \Exception('Cannot write/load source for class '.$class.' in '.$cacheFile);
+	   }
 			
   }
 	
