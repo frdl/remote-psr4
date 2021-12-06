58bd790dabedfc5644ead654839b092ce4d69a9c
Till Wehowski <github@webfan.de>
2021-12-06 20:09:43 +0100

    Update RemoteFallbackLoader.php

--- src/implementations/autoloading/RemoteFallbackLoader.php
+++ src/implementations/autoloading/RemoteFallbackLoader.php
@@ -19,6 +19,132 @@ class RemoteFallbackLoader {
 	protected static $instances = [];
 	protected static $classmap = [
 	    \Wehowski\Gist\Http\Response\Helper::class => 'https://gist.githubusercontent.com/wehowski/d762cc34d5aa2b388f3ebbfe7c87d822/raw/5c3acdab92e9c149082caee3714f0cf6a7a9fe0b/Wehowski%255CGist%255CHttp%255CResponse%255CHelper.php?cache_bust=${salt}',
+				'GuzzleHttp\\uri_template' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\describe_type' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\headers_from_lines' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\debug_resource' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\choose_handler' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\default_user_agent' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\default_ca_bundle' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\normalize_header_keys' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\is_host_in_noproxy' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\json_decode' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		'GuzzleHttp\\json_encode' => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\choose_handler',
+		
+	\GuzzleHttp\LoadGuzzleFunctionsForFrdl::class => 'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\LoadGuzzleFunctionsForFrdl',
+		 
+		'GuzzleHttp\\Psr7\\uri_for' =>  'https://03.webfan.de/install/?salt=${salt}&version=${version}&source=GuzzleHttp\Psr7\uri_for',
+		
+	
+		
+	\webfan\hps\Format\DataUri::class => 'https://03.webfan.de/install/?salt=${salt}&source=webfan\hps\Format\DataUri',
+	'frdl\\Proxy\\' => 'https://raw.githubusercontent.com/frdl/proxy/master/src/${class}.php?cache_bust=${salt}',
+	 \frdlweb\Thread\ShutdownTasks::class => 'https://raw.githubusercontent.com/frdl/shutdown-helper/master/src/ShutdownTasks.php?cache_bust=${salt}',
+
+		
+		'Fusio\\Adapter\\Webfantize\\' => 'https://raw.githubusercontent.com/frdl/fusio-adapter-webfantize/master/src/${class}.php?cache_bust=${salt}',
+		
+		//frdl\implementation\psr4 
+		//Nette\Loaders\FrdlRobotLoader
+		'@frdl\\implementation\\Autoload\\Local' =>  \Webfan\Autoload\ClassMapGenerator::class,
+		/* \Nette\Loaders\FrdlRobotLoader::class, */
+		'@frdl\\implementation\\Autoload\\Remote' => __CLASS__,
+		'@frdl\\implementation\\Build\\ClassMap' => \Webfan\Autoload\ClassMapGenerator::class,
+		'@frdl\\implementation\\Autoload' => __CLASS__,
+		
+		/*
+		 \Webfan\Autoupdate\SVLClassInterface::class=>
+		    'https://cdn.frdl.io/@webfan3/stubs-and-fixtures/classes/frdl/implementation/psr4/RemoteAutoloader',
+		  \Webfan\Autoupdate\SVLClassTrait::class =>  
+		     'https://cdn.frdl.io/@webfan3/stubs-and-fixtures/classes/frdl/implementation/psr4/RemoteAutoloader',
+			 */
+			 
+    // NAMESPACES
+    // Zend Framework components
+    '@Zend\\AuraDi\\Config' => 'Laminas\\AuraDi\\Config',
+    '@Zend\\Authentication' => 'Laminas\\Authentication',
+    '@Zend\\Barcode' => 'Laminas\\Barcode',
+    '@Zend\\Cache' => 'Laminas\\Cache',
+    '@Zend\\Captcha' => 'Laminas\\Captcha',
+    '@Zend\\Code' => 'Laminas\\Code',
+    '@ZendCodingStandard\\Sniffs' => 'LaminasCodingStandard\\Sniffs',
+    '@ZendCodingStandard\\Utils' => 'LaminasCodingStandard\\Utils',
+    '@Zend\\ComponentInstaller' => 'Laminas\\ComponentInstaller',
+    '@Zend\\Config' => 'Laminas\\Config',
+    '@Zend\\ConfigAggregator' => 'Laminas\\ConfigAggregator',
+    '@Zend\\ConfigAggregatorModuleManager' => 'Laminas\\ConfigAggregatorModuleManager',
+    '@Zend\\ConfigAggregatorParameters' => 'Laminas\\ConfigAggregatorParameters',
+    '@Zend\\Console' => 'Laminas\\Console',
+    '@Zend\\ContainerConfigTest' => 'Laminas\\ContainerConfigTest',
+    '@Zend\\Crypt' => 'Laminas\\Crypt',
+    '@Zend\\Db' => 'Laminas\\Db',
+    '@ZendDeveloperTools' => 'Laminas\\DeveloperTools',
+    '@Zend\\Di' => 'Laminas\\Di',
+    '@Zend\\Diactoros' => 'Laminas\\Diactoros',
+    '@ZendDiagnostics\\Check' => 'Laminas\\Diagnostics\\Check',
+    '@ZendDiagnostics\\Result' => 'Laminas\\Diagnostics\\Result',
+    '@ZendDiagnostics\\Runner' => 'Laminas\\Diagnostics\\Runner',
+    '@Zend\\Dom' => 'Laminas\\Dom',
+    '@Zend\\Escaper' => 'Laminas\\Escaper',
+    '@Zend\\EventManager' => 'Laminas\\EventManager',
+    '@Zend\\Feed' => 'Laminas\\Feed',
+    '@Zend\\File' => 'Laminas\\File',
+    '@Zend\\Filter' => 'Laminas\\Filter',
+    '@Zend\\Form' => 'Laminas\\Form',
+    '@Zend\\Http' => 'Laminas\\Http',
+    '@Zend\\HttpHandlerRunner' => 'Laminas\\HttpHandlerRunner',
+    '@Zend\\Hydrator' => 'Laminas\\Hydrator',
+    '@Zend\\I18n' => 'Laminas\\I18n',
+    '@Zend\\InputFilter' => 'Laminas\\InputFilter',
+    '@Zend\\Json' => 'Laminas\\Json',
+    '@Zend\\Ldap' => 'Laminas\\Ldap',
+    '@Zend\\Loader' => 'Laminas\\Loader',
+    '@Zend\\Log' => 'Laminas\\Log',
+    '@Zend\\Mail' => 'Laminas\\Mail',
+    '@Zend\\Math' => 'Laminas\\Math',
+    '@Zend\\Memory' => 'Laminas\\Memory',
+    '@Zend\\Mime' => 'Laminas\\Mime',
+    '@Zend\\ModuleManager' => 'Laminas\\ModuleManager',
+    '@Zend\\Mvc' => 'Laminas\\Mvc',
+    '@Zend\\Navigation' => 'Laminas\\Navigation',
+    '@Zend\\Paginator' => 'Laminas\\Paginator',
+    '@Zend\\Permissions' => 'Laminas\\Permissions',
+    '@Zend\\Pimple\\Config' => 'Laminas\\Pimple\\Config',
+    '@Zend\\ProblemDetails' => 'Mezzio\\ProblemDetails',
+    '@Zend\\ProgressBar' => 'Laminas\\ProgressBar',
+    '@Zend\\Psr7Bridge' => 'Laminas\\Psr7Bridge',
+    '@Zend\\Router' => 'Laminas\\Router',
+    '@Zend\\Serializer' => 'Laminas\\Serializer',
+    '@Zend\\Server' => 'Laminas\\Server',
+    '@Zend\\ServiceManager' => 'Laminas\\ServiceManager',
+    '@ZendService\\ReCaptcha' => 'Laminas\\ReCaptcha',
+    '@ZendService\\Twitter' => 'Laminas\\Twitter',
+    '@Zend\\Session' => 'Laminas\\Session',
+    '@Zend\\SkeletonInstaller' => 'Laminas\\SkeletonInstaller',
+    '@Zend\\Soap' => 'Laminas\\Soap',
+    '@Zend\\Stdlib' => 'Laminas\\Stdlib',
+    '@Zend\\Stratigility' => 'Laminas\\Stratigility',
+    '@Zend\\Tag' => 'Laminas\\Tag',
+    '@Zend\\Test' => 'Laminas\\Test',
+    '@Zend\\Text' => 'Laminas\\Text',
+    '@Zend\\Uri' => 'Laminas\\Uri',
+    '@Zend\\Validator' => 'Laminas\\Validator',
+    '@Zend\\View' => 'Laminas\\View',
+    '@ZendXml' => 'Laminas\\Xml',
+    '@Zend\\Xml2Json' => 'Laminas\\Xml2Json',
+    '@Zend\\XmlRpc' => 'Laminas\\XmlRpc',
+    '@ZendOAuth' => 'Laminas\\OAuth',	
+		
+		
+		//https://raw.githubusercontent.com/elastic/elasticsearch-php/v7.12.0/src/autoload.php
+	'Elasticsearch\\' => 'https://raw.githubusercontent.com/elastic/elasticsearch-php/v7.12.0/src/${class}.php?cache_bust=${salt}',
+		
+		\WeidDnsConverter::class =>
+		'https://raw.githubusercontent.com/frdl/oid2weid/master/src/WeidDnsConverter.php?cache_bust=${salt}',
+		\WeidHelper::class =>
+		'https://raw.githubusercontent.com/frdl/oid2weid/master/src/WeidHelper.php?cache_bust=${salt}',
+		\WeidOidConverter::class =>
+		'https://raw.githubusercontent.com/frdl/oid2weid/master/src/WeidOidConverter.php?cache_bust=${salt}',
 	];
 	
 	protected static $prefixes = [];
@@ -37,6 +163,9 @@ class RemoteFallbackLoader {
 		 $server  = 'https://startdir.de/install/?salt=${salt}&source=${class}';   
 	   }
 	   
+	   if(null===$classMap){
+		 $classMap  = self::$classmap;   
+	   }
 	        $this->withSalt($salted);
 	        $this->withClassmap($classMap);
 		$this->allowFromSelfOrigin = $allowFromSelfOrigin;
